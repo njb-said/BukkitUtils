cfc626dcb3fc42cac5ee19d4ac5929bbfcbc74e0
njb_said <njb_said@hotmail.co.uk>
2015-03-13 21:38:50 +0000

    First commit

---
+++ src/main/java/me/imnjb/bukkitutils/PlayerUtils.java
@@ -0,0 +1,68 @@
+package me.imnjb.bukkitutils;
+
+import org.bukkit.entity.Player;
+import org.bukkit.potion.PotionEffect;
+
+/**
+ * A list of player related utils
+ *
+ * @author njb_said
+ * @author CoderCake
+ */
+public class PlayerUtils {
+
+    /**
+     * Clear a players inventory + removes all their potion effects
+     *
+     * @param player player to clear
+     */
+    public static void clearInventory(Player player) {
+        player.getInventory().clear();
+        player.getInventory().setArmorContents(null);
+
+        for(PotionEffect effect : player.getActivePotionEffects()) {
+            player.removePotionEffect(effect.getType());
+        }
+
+        update(player);
+    }
+
+    public static void resetXP(Player player) {
+        player.setExp(0.0f);
+        player.setLevel(0);
+    }
+
+    /**
+     * Heal/feed the player
+     *
+     * @param player player to heal/feed
+     */
+    public static void heal(Player player) {
+        player.setHealth(player.getMaxHealth());
+        player.setFoodLevel(20);
+    }
+
+    /**
+     * Uses {@link #clearInventory(Player)}, {@link #resetXP(Player)} and {@link #heal(Player)}
+     */
+    public static void reset(Player player) {
+        clearInventory(player);
+        heal(player);
+        resetXP(player);
+
+        player.setFallDistance(0.0f);
+        player.setFireTicks(0);
+    }
+
+    /**
+     * Call the update method
+     * <p>
+     * This used to be deprecated in bukkit which gave this method a purpose
+     *
+     * @author CoderCake
+     */
+    public static void update(Player player) {
+        player.updateInventory();
+    }
+
+}
